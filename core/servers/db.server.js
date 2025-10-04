604c481f1f88924f776b8bea6808007352e65d85
Sergio <comesanapoloniosergiobrais@gmail.com>
2025-10-04 11:46:34 +0200

    deploy app

---
+++ core/servers/db.server.js
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb';
+let client;
+const connect = async (connectionURL) => {
+    client = new MongoClient(connectionURL);
+    await client.connect();
+    dbServer.db = client.db();
+};
+
+const disconnect = async () => {
+    await client.close();
+};
+
+export let dbServer = {
+    connect,
+    disconnect,
+    db: undefined,
+};
